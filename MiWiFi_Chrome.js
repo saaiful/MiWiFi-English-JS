166ea986141aa482282294d1ba385e0703eecf30
Saiful Islam <saaifulislam@gmail.com>
2015-09-05 14:25:04 +0600

    Automatic Update to Version 3

--- MiWiFi_Chrome.js
+++ MiWiFi_Chrome.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MiWiFi in English
 // @namespace    http://saiful.im/miwifi/
-// @version      2
+// @version      3
 // @description  Xiaomi AC Router Translation in English
 // @author       Saiful Islam
 // @match        http://miwifi.com/*
